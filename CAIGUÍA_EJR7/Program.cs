43695a5f7316e541823a89979349537e6c3ff0f7
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-28 19:58:53 -0300

    EJERCICIO 7

---
+++ CAIGUÍA_EJR7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUÍA_EJR7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite al usuario el ingreso de su nombre y presente el mensaje “Hola [nombre]”.
+
+            Console.Write("Ingrese su nombre: ");
+            string NOMBRE = Console.ReadLine();
+
+            Console.Write($"Hola {NOMBRE}");
+
+            Console.ReadKey();
+        }
+    }
+}
