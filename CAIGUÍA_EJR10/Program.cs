03ace14470249d1b9a4c2b6597fdc8d81559064d
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-29 18:44:40 -0300

    ejercicio 10

---
+++ CAIGUÍA_EJR10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUÍA_EJR10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Por favor ingrese texto: ");
+                string ingreso = Console.ReadLine();
+
+                Console.WriteLine($"Su ingreso es: {ingreso.ToLower()}");
+
+            } while (false);
+
+
+            Console.ReadKey();
+        }
+    }
+}
