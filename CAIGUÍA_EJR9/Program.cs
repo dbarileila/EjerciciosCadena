8b7de95bfcc73f5fe301a5d442046c6afaf163d5
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-29 18:40:03 -0300

    ejercicio 9

---
+++ CAIGUÍA_EJR9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUÍA_EJR9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite un ingreso al usuario y lo presente en mayúsculas.
+
+            do
+            {
+                Console.Write("Por favor ingrese texto: ");
+                string ingreso = Console.ReadLine();
+
+                Console.WriteLine($"Su ingreso es: {ingreso.ToUpper()}");
+
+            } while (false);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
