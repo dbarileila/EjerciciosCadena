8c466eb39ae16ef44311ad89e3273da604396ee1
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-28 21:31:43 -0300

    Ejercicio 8

---
+++ CAIGUÍA_EJR8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUÍA_EJR8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Solicite al usuario el ingreso de una frase y presente un mensaje indicando si alguna contiene la
+            palabra “fin”.*/
+
+            
+            do
+            {
+                Console.Write("Por favor ingrese una frase: ");
+                 string frase = Console.ReadLine();
+
+                if (frase.Contains("fin"))
+                {
+                    Console.Write($"Su frase ingresada fue: {frase} ");
+                    break;
+
+                }
+              
+
+
+            } while (true);
+
+            Console.ReadKey();
+
+     
+        }
+    }
+}
