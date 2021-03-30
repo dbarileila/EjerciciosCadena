e8fddf600c39021838e71affb5195ba28730f820
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-29 22:11:38 -0300

    ejercicio 11 con regex

---
+++ CAIGUÍA_EJR11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CAIGUÍA_EJR11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Reemplace en una cadena ingresada por el usuario todas las vocales acentuadas por vocales 
+             * sin acento y presente el resultado.*/
+
+            do
+            {
+                Console.Write("Ingrese un texto con las vocales acentuadas: ");
+                string ingreso = Console.ReadLine();
+
+                
+                
+
+                string palabrasinTildes = Regex.Replace(ingreso.Normalize(NormalizationForm.FormD), @"[^0-9A-Za-z\s]", "",
+                RegexOptions.None);
+                
+             
+                Console.WriteLine($"Su ingreso sin acento: {palabrasinTildes}");
+
+
+            } while (false);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
