9dce2b5ca4dae8ff26ab086eef25b6ab4be6c1f5
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-04-04 12:55:22 -0300

    ejercicio 14

---
+++ CAIGUIA_EJR14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+
+namespace CAIGUIA_EJR14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de dos frases y determine si son iguales, sin diferenciar mayúsculas de minúsculas.
+
+            string frase1;
+            string frase2;
+            bool igualdad = true;
+
+            do
+            {
+                Console.Write("Ingrese una frase: ");
+                frase1 = Console.ReadLine();
+
+                Console.Write("Ingrese la misma frase: ");
+                frase2 = Console.ReadLine();
+
+                if (frase1.Equals(frase2, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine($"Usted ingreso dos frases iguales. Frase 1: '{frase1}' y Frase 2: '{frase2}' ");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No ingreso la misma frase.");
+                    
+                }
+
+            } while (igualdad);
+
+            Console.ReadKey();
+
+        }
+    }
+}
