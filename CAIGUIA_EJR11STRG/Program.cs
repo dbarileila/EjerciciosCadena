40a18178e2d01affb971d1711b9e0306f143f341
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-04-04 13:20:50 -0300

    ejercicio 11 string

---
+++ CAIGUIA_EJR11STRG/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIA_EJR11STRG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Reemplace en una cadena ingresada por el usuario todas las vocales 
+            acentuadas por vocales sin acento y presente el resultado.*/
+
+              
+             Console.Write("Ingrese texto acento: ");
+             string ingreso = Console.ReadLine();
+
+                string[] ingresoConAcento = new string[] {"á", "é", "í", "ó", "ú", "Á", "É", "Í", "Ó", "Ú"};
+                string[] ingresoSinAcento = new string[] { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+
+                for (int i = 0; i < 10; i++)
+                {
+                    ingreso = ingreso.Replace(ingresoConAcento[i], ingresoSinAcento[i]);
+                }
+
+            Console.WriteLine($"Usted ingreso {ingreso}");
+            
+            Console.ReadKey();
+
+        }
+    }
+}
